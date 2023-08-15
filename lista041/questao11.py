1cc7b849ea2eb838b42dd32a7d50204b5a767860
Gustavo Baptista <chustaffus777@gmail.com>
2023-08-15 11:05:52 -0300

    Questão 11 solucionada, falta a 13

--- lista041/questao11.py
+++ lista041/questao11.py
@@ -2,8 +2,11 @@
 Desenvolver um programa que pergunte um número inteiro de 3 algarismos e apresente como resultado
 somente o algarismo das centenas.
 '''
-print("A seguir, insira números de 3 algarismos!")
-n1 = int(input("Insira um número:"))
-n2 = int(input("Mais um número:"))
-n3 = int(input("Agora um último número:"))
+print("A seguir, insira um número de 3 algarismos!")
+n1 = int(input("Coloque um número:"))
 
+if (n1 < 100):
+    print("Esse número não corresponde ao formato exigido.")
+
+else:
+  print(f" O algarismo que simboliza as centenas é equivalente a {n1//100}.")
--- lista041/questao13.py
+++ lista041/questao13.py
@@ -7,5 +7,27 @@ n1 = float(input("Insira o 1º número:"))
 n2 = float(input("Insira o 2º número:"))
 n3 = float(input("Insira o 3º número:"))
 
+menor =  n1
+if (menor < n2):
+        menor = n2
+
+if (menor < n3):
+        menor = n3
+
+
+maior = n1
+
+if (maior < n2):
+        maior = n2
+
+if (maior < n3):
+        maior = n3
+
+
+print(f"{menor},{inter},{maior}.")
+
+
+
+
 
 
